f22317830a4baeecba51e2a157376b53d3422698
CyberCadence <diljithv7@gmail.com>
2022-09-18 12:12:50 +0530

    done

---
+++ linux/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,15 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "/home/diljith/snap/flutter/common/flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "/home/diljith/Flutter projects/TEST/sample" PROJECT_DIR)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=/home/diljith/snap/flutter/common/flutter"
+  "PROJECT_DIR=/home/diljith/Flutter projects/TEST/sample"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=/home/diljith/Flutter projects/TEST/sample/.dart_tool/package_config.json"
+  "FLUTTER_TARGET=/home/diljith/Flutter projects/TEST/sample/lib/main.dart"
+)
